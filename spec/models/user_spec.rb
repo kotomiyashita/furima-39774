b1f90974aeebe183f42a3e8e2f26be65442ed89b
kotomiyashita <kotomi.owo@gmail.com>
2023-08-15 21:19:36 +0900

    指摘箇所の修正２

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe User, type: :model do
     it 'emailに@が含まれていない場合登録できない' do
       @user.email = 'test.com'
       @user.valid?
-      expect(@user.errors.full_messages).to include("")
+      expect(@user.errors.full_messages).to include("Email is invalid")
     end
     it 'passwardが空では登録できない' do
       @user.password = ''
