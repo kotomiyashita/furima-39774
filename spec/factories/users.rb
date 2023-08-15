91c83a2d6f18da82306722e488a08ae6644ff63b
kotomiyashita <kotomi.owo@gmail.com>
2023-08-15 18:25:28 +0900

    モデル単体テストを記述

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.email}
+    password              {'aaa000'}
+    password_confirmation {password}
+    family_name           {'山田'}
+    first_name            {'隆太郎'}
+    family_name_kana      {'ヤマダ'}
+    first_name_kana       {'リクタロウ'}
+    date_of_birth         {'2000-01-01'}
+  end
+end
