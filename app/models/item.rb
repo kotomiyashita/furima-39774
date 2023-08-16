9edc5b1de95e3772177cb6f11767d1771de4e25d
kotomiyashita <kotomi.owo@gmail.com>
2023-08-16 21:00:05 +0900

    エラーメッセージが重複しないよう修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,11 +10,11 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :estimated_shipping
 
-  validates :title, :price, :description, presence: true
+  validates :title, :description, presence: true
 
   validates :price, presence: true,
-                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 },
-                    format: {with: /\A\d+\z/}
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999,  allow_blank: true },
+                    format: { with: /\A\d+\z/, allow_blank: true }
 
   validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :estimated_shipping_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
 end
