96ee70a8a531b64acdaf8d636a7799708cea80d3
kotomiyashita <kotomi.owo@gmail.com>
2023-08-17 10:39:48 +0900

    任意修正箇所を修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,72 +4,75 @@ RSpec.describe Item, type: :model do
   before do
     @item = FactoryBot.build(:item)
   end
-  describe '商品出品ができる時' do
-    it '商品出品できる' do
-      expect(@item).to be_valid
+  
+  describe '商品出品' do
+    context '商品出品ができる時' do
+      it '商品出品できる' do
+        expect(@item).to be_valid
+      end
     end
-  end
 
-  describe '商品出品ができない時' do
-    it '商品名が空では投稿できない' do
-      @item.title = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Title can't be blank")
-    end
-    it '商品説明が空では投稿できない' do
-      @item.description = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Description can't be blank")
-    end
-    it 'カテゴリーが未選択では投稿できない' do
-      @item.category_id = '1'
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Category can't be blank")
-    end
-    it '商品状態が未選択では投稿できない' do
-      @item.condition_id = '1'
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Condition can't be blank")
-    end
-    it '配送料が未選択では投稿できない' do
-      @item.shipping_fee_id = '1'
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
-    end
-    it '配送元地域が未選択では投稿できない' do
-      @item.prefecture_id = '1'
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Prefecture can't be blank")
-    end
-    it '配送目安が未選択では投稿できない' do
-      @item.estimated_shipping_id = '1'
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Estimated shipping can't be blank")
-    end
-    it '販売価格が空では投稿できない' do
-      @item.price = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Price can't be blank")
-    end
-    it '販売価格が300未満では投稿できない' do
-      @item.price = '299'
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
-    end
-    it '販売価格が10000000以上では投稿できない' do
-      @item.price = '10000000'
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
-    end
-    it '販売価格が全角では投稿できない' do
-      @item.price = '１００００'
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Price is not a number')
-    end
-    it 'ユーザーが紐付いていなければ投稿できない' do
-      @item.user = nil
-      @item.valid?
-      expect(@item.errors.full_messages).to include('User must exist')
+    context '商品出品ができない時' do
+      it '商品名が空では投稿できない' do
+        @item.title = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Title can't be blank")
+      end
+      it '商品説明が空では投稿できない' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it 'カテゴリーが未選択では投稿できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it '商品状態が未選択では投稿できない' do
+        @item.condition_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it '配送料が未選択では投稿できない' do
+        @item.shipping_fee_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
+      end
+      it '配送元地域が未選択では投稿できない' do
+        @item.prefecture_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '配送目安が未選択では投稿できない' do
+        @item.estimated_shipping_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Estimated shipping can't be blank")
+      end
+      it '販売価格が空では投稿できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it '販売価格が300未満では投稿できない' do
+        @item.price = '299'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      end
+      it '販売価格が10000000以上では投稿できない' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+      end
+      it '販売価格が全角では投稿できない' do
+        @item.price = '１００００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+      it 'ユーザーが紐付いていなければ投稿できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
     end
   end
 end
