6a7ca57463fcfcabe07ff8108debcd2fbad6ef6b
kotomiyashita <kotomi.owo@gmail.com>
2023-08-20 13:16:19 +0900

    OrderShippinginfoモデルの単体テスト

--- app/models/order_shippinginfo.rb
+++ app/models/order_shippinginfo.rb
@@ -1,24 +1,28 @@
 class OrderShippinginfo
+
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :item_id, :user_id
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :item_id, :user_id,:item_id
   attr_accessor :token
 
-  validates :token, presence: true
+  with_options presence: true do
+    validates :token
 
-  validates :postal_code, presence: true,
-            format: { with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" , allow_blank: true }
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" , allow_blank: true }
 
-  validates :prefecture_id, presence: true,
-  numericality: { other_than: 1, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
-  validates :city, :address, presence: true
+    validates :city
+    validates :address
 
-  validates :phone_number, presence: true,
-            format: { with: /\A[0-9]+\z/, message: "is invalid. Input only number" , allow_blank: true },
-            length: { minimum: 10, maximum: 11 , allow_blank: true }
+    validates :phone_number, format: { with: /\A[0-9]+\z/, message: "is invalid. Input only number" , allow_blank: true },
+                            length: { minimum: 10, maximum: 11 , allow_blank: true }
+
+    validates :user_id
+    validates :item_id
+  end
 
   def save
-    order = Order.create(item_id:item_id, user_id: user_id)
+    order = Order.create(item_id: item_id, user_id: user_id)
     Shippinginfo.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, order_id: order.id)
   end
 end
---
+++ spec/factories/order_shippinginfos.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :order_shippinginfo do
+    token         {"tok_abcdefghijk00000000000000000"}
+    postal_code   { "123-4567" }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    city          { Faker::Lorem.sentence }
+    address       { Faker::Lorem.sentence }
+    building      { Faker::Lorem.sentence }
+    phone_number  { Faker::Number.number(digits: rand(10..11)) }
+  end
+end
